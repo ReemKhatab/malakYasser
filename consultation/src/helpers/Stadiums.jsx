b6fd52fcd448a6e5b81ae6a6d65036952fec542c
Malakyasser8 <malakyasser8@gmail.com>
2023-12-31 16:23:41 +0200

    finished create match

--- consultation/src/helpers/Stadiums.jsx
+++ consultation/src/helpers/Stadiums.jsx
@@ -1,27 +1,38 @@
-export const Stadiums = [
-    {
-      id: 1,
-      name: "Cairo Stadium",
-      rows: 5,
-      columns:6,
-      numberOfSeats:30,
-    },
-    {
-      id: 2,
-      name: "Alex Stadium",
-      activated: false,
-      rows: 5,
-      columns:6,
-      numberOfSeats:30,
-    },
-    {
-      id: 3,
-      name: "Suez Stadium",
-      activated: true,
-      rows: 5,
-      columns:6,
-      numberOfSeats:30,
-    },
-   
-  ];
-  
+// export const Stadiums = [
+//     {
+//       name: "Cairo Stadium",
+//       rows: 5,
+//       columns:6,
+//       numberOfSeats:30,
+//     },
+//     {
+//       name: "Alex Stadium",
+//       rows: 5,
+//       columns:6,
+//       numberOfSeats:30,
+//     },
+//     {
+//       name: "Suez Stadium",
+//       rows: 5,
+//       columns:6,
+//       numberOfSeats:30,
+//     },
+//   ];
+
+import axios from "axios";
+
+let Stadiums = [];
+
+const fetchStadiums = () => {
+  return axios
+    .get("http://localhost:8808/EFA_manager/create_new_match/get_all_stadiums")
+    .then(function (response) {
+      Stadiums = response.data;
+      console.log(Stadiums);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
+export { Stadiums, fetchStadiums };
