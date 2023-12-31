5b263c5b34ad1f68c5fd46dbdbd63a6951d8b201
ReemKhatab <reem.khatab02@eng-st.cu.edu.eg>
2023-12-31 15:22:28 +0200

    customer view matches backend

--- consultation/src/helpers/Teams.jsx
+++ consultation/src/helpers/Teams.jsx
@@ -1,18 +1,75 @@
 export const Teams = [
   {
-    teamname: "Ahly",
+    teamname: "Al-Ahly",
     logo: "/logos/Alahly.png",
+  },
+  {
+    teamname: "Al-Ittihad",
+    logo: "/logos/alittihad.png",
+  },
+  {
+    teamname: "Al-Masry",
+    logo: "/logos/almasry.png",
+  },
+  {
+    teamname: "Al-Mokawloon Al-Arab",
+    logo: "/logos/almokawloon.png",
+  },
+  {
+    teamname: "Al-Sharkeyah",
+    logo: "/logos/alsharkeyah.png",
+  },
+  {
+    teamname: "Cermica Cleopatra",
+    logo: "/logos/cermicacleopatra.jpg",
+  },
+  {
+    teamname: "El-Geish",
+    logo: "/logos/elgeish.jpg",
+  },
+  {
+    teamname: "El-Gouna",
+    logo: "/logos/elgouna.png",
+  },
+  {
+    teamname: "Enppi",
+    logo: "/logos/enppi.png",
+  },
+  {
+    teamname: "Future",
+    logo: "/logos/future.jpg",
+  },
+  {
+    teamname: "Ghazl-El-Mahalla",
+    logo: "/logos/ghazlelmahalla.png",
+  },
+  {
+    teamname: "Ismaily",
+    logo: "/logos/ismaily.png",
+  },
+  {
+    teamname: "Misr El-Makkasa",
+    logo: "/logos/misrelmakkasa.jpg",
+  },
+  {
+    teamname: "National Bank",
+    logo: "/logos/nationalbank.jpg",
+  },
+  {
+    teamname: "Pharco",
+    logo: "/logos/pharco.png",
+  },
+  {
+    teamname: "Pyramids",
+    logo: "/logos/pyramids.jpg",
+  },
+  {
+    teamname: "Smouha",
+    logo: "/logos/smouha.png",
   },
   {
     teamname: "Zamalek",
     logo: "/logos/zamalek.png",
   },
-  {
-    teamname: "Elgouna",
-    logo: "/logos/elgouna.png",
-  },
-  {
-    teamname: "Etihad",
-    logo: "/logos/alittihad.png",
-  },
+  
 ];
